bfacae15dd4c4e6ad2cbb25b5862886eb2e39d9f
Nick Malaguti <nmalaguti@palantir.com>
2015-03-07 12:54:45 -0500

    Add configuration for where Gruntfile.js is located
    Add new 'grunt' extension with option workDir. By default workDir =
    projectDir.

--- src/main/groovy/com/moowork/gradle/grunt/GruntTask.groovy
+++ src/main/groovy/com/moowork/gradle/grunt/GruntTask.groovy
@@ -16,13 +16,14 @@ class GruntTask
     @Override
     void exec()
     {
-        def localGrunt = this.project.file( GRUNT_SCRIPT )
+        def localGrunt = this.project.file( new File( this.project.node.nodeModulesDir, GRUNT_SCRIPT ) )
         if ( !localGrunt.isFile() )
         {
             throw new GradleException(
                 "Grunt-CLI not installed in node_modules, please first run 'gradle ${GruntPlugin.GRUNT_INSTALL_NAME}'" )
         }
 
+        setWorkingDir( this.project.grunt.workDir )
         setScript( localGrunt )
         super.exec()
     }
